4659c1732c4b8a9d231bd07315693e02733e7b4e
Andrey A. Konovalov <drvtiny@gmail.com>
2019-11-01 15:31:39 +0300

    support for http ports with(out) SSL layer on socket

--- tcp_tester.cr
+++ tcp_tester.cr
@@ -1,28 +1,38 @@
 require "socket"
 require "json"
+require "http/server"
 module TcpTester
   VERSION = "0.1.0"
-  DFLT_WORK_MODE = "client"
+  DFLT_WORK_MODE = "server"
   DFLT_BIND_HOST = "0.0.0.0"
   DFLT_RMT_HOST = "localhost"
 
   class PortRec
     JSON.mapping(
-      port: {type: Int32, nilable: false},
-      remote: {type: String, nilable: false, default: TcpTester::DFLT_RMT_HOST},
-      listen: {type: String, nilable: false, default: TcpTester::DFLT_BIND_HOST}
+      port:			{type: Int32,		nilable: false},
+      remote: 	{type: String, 	nilable: false, default: TcpTester::DFLT_RMT_HOST},
+      listen: 	{type: String, 	nilable: false, default: TcpTester::DFLT_BIND_HOST},
+      type: 		{type: String, 	nilable: false, default: "ftp"},
+      use_ssl: 	{type: Bool, nilable: false, default: false}
     )
   end
 
   unless conf_file = ARGV[0]?
     raise "No config file specified, you will burn in Hell!"
   end
+  
+	lib C
+	  fun geteuid() : UInt32
+	end
+
+	euid = C.geteuid
+	host = System.hostname.match(/^([^.]+)/).not_nil![0]
 
   descr = Array(PortRec).from_json(File.open(conf_file).gets_to_end)
   work_mode = ARGV[1]? || DFLT_WORK_MODE
-  ch = Channel(Nil).new  
   case work_mode
   when "client"
+  	ch_client = Channel(Nil).new
     descr.each do |e|
       spawn do
         tcp_client = TCPSocket.new(e.remote, e.port)
@@ -30,36 +40,74 @@ module TcpTester
         response = tcp_client.gets
         puts "message=<<#{response}>>"
         tcp_client.close
-        ch.send(nil)
+        ch_client.send(nil)
       end
     end
+    descr.size.times { ch_client.receive }
   when "server"
-        descr.each do |e|
-          spawn do
-            tcp_server = TCPServer.new(e.listen, e.port)
-            puts "listen on #{e.listen}:#{e.port}"
-            while client = tcp_server.accept?
-              spawn do
-                if c = client.not_nil!
-                	puts "write FTP header"
-                	c.puts "220 OK"
-                	while ans = c.gets
-                    if ans.match(/^QUIT/)
-                      break
-                    else
-                      c.puts "OK"
-                    end
-                	end
-                	c.close
-                end
-              end
-            end
-            ch.send(nil)
-          end
+  	raise "cant bind to privileged ports, insufficient privileges" if euid > 0 && descr.find {|e| e.port < 1024 }
+  	ch = {http: Channel(Nil).new, other: Channel(Nil).new}
+  	if flHasHTTP = descr.find {|e| e.type == "http"}
+  	begin
+      ssl_conf 										= OpenSSL::SSL::Context::Server.new
+      ssl_conf.certificate_chain 	= "CA/#{host}-www.crt"
+      ssl_conf.private_key				= "CA/private/#{host}-www.key"
+      
+			spawn do
+        http_server = HTTP::Server.new do |ctx|
+          ctx.response.content_type = "text/plain"
+          ctx.response.print "PONG"
+        rescue ex
+          puts "ERROR: Exception of type #{ex.class} catched: #{ex.message}"
         end
+        
+        descr.reject {|e| e.type != "http" }.each do |e|
+          unless e.use_ssl
+            http_server.bind_tcp "0.0.0.0", e.port
+          else
+            http_server.bind_tls "0.0.0.0", e.port, ssl_conf
+          end
+          puts "HTTPServer listens on http#{e.use_ssl ? "s" : ""}://0.0.0.0:#{e.port}"
+        end
+        http_server.listen
+        ch[:http].send(nil)
+      rescue ex
+        puts "ERROR: Exception of type #{ex.class} catched: #{ex.message}"
+	    end # <- spawn HTTP::Server
+    rescue ex
+      puts "ERROR: Exception of type #{ex.class} catched: #{ex.message}"
+    end
+		end
+		other_cnt = 0
+		if flHasNoHTTP = descr.find {|e| e.type != "http"}
+			descr.reject {|e| e.type == "http" }.each do |e|
+				spawn do
+		      tcp_server = TCPServer.new(e.listen, e.port)
+		      other_cnt += 1
+  	      puts "TCPServer listen on #{e.listen}:#{e.port}"
+          while client = tcp_server.accept?
+            spawn do
+              if c = client.not_nil!
+                puts "write FTP header"
+                c.puts "220 OK"
+                while ans = c.gets
+                  if ans.match(/^QUIT/)
+                    break
+                  else
+                    c.puts "OK"
+                  end
+                end
+                c.close
+              end
+            end # <- spawn (client connection)
+          end # <- while tcp_server.accept          
+        	ch.[:other].send(nil)
+        end # spawn (server itself)
+      end # ports enumeration
+    end # if some ports is not http
+    ch[:http].receive if flHasHTTP
+    other_cnt.times { ch[:other].receive } if flHasNoHTTP
   else
-        raise "Unknown work mode: #{work_mode}"
+  	raise "Unknown work mode: #{work_mode}"
   end
-  
-  descr.size.times { ch.receive }
 end
